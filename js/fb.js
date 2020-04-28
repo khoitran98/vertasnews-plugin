26f977c386a100a1f6b77f34245d209d8ba41bda
Khoi Tran <trandangkhoi1998@gmail.com>
2020-04-28 02:23:03 -0500

    Test firebase

---
+++ js/fb.js
@@ -0,0 +1,16 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+
+// Initialize Firebase
+var config = {
+  apiKey: '',
+  authDomain: '',
+  databaseURL: '',
+  projectId: '',
+  storageBucket: '',
+  messagingSenderId: ''
+};
+firebase.initializeApp(config);
+const db = firebase.firestore();
+
+export default db;
